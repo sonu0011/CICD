cfc1a923ff085cc7f46bd711d7ac14a6a3f914ca
sonu kumar <sonu1633900@gmail.com>
2024-09-14 12:11:22 +0530

    sonar

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,13 @@ plugins {
     id("com.android.application") version "8.2.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
+    id("org.sonarqube") version "4.4.1.3373"
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "cicd-1111_cicd")
+        property("sonar.organization", "cicd-1111")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
